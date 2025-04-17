d6ca196f72385f230810737cedf7088e157ecb9c
Jimb Esser <wasteland@gmail.com>
2025-04-17 07:53:15 -0700

    Add build_hide to all test and demo assets

--- src/client/crawler_entity_client.ts
+++ src/client/crawler_entity_client.ts
@@ -117,6 +117,7 @@ export interface EntityCrawlerClient extends EntityBaseClient {
   is_player: boolean;
   is_enemy: boolean;
   blocks_player: boolean;
+  build_hide?: boolean;
   map_icon?: string;
 }
 
